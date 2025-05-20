8f623a76943faa60d3483716911b79290edcad24
MO SAIED <mosaied00786@gmail.com>
2025-05-20 23:51:47 +0530

    Create List and perform some operation

--- JavaCollection/src/ListInterface/ArrayListDemo.java
+++ JavaCollection/src/ListInterface/ArrayListDemo.java
@@ -1,9 +1,26 @@
 package ListInterface;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListDemo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Words");
+		
+		List<String> product = new ArrayList<>();
+		
+		product.add("Laptop");
+		product.add("SmatPhone");
+		product.add("HeadPhone");
+		
+		System.out.println("Electronics Product is : "+product);// Printing all the project
+		
+		System.out.println("Get product on index 2 "+product.get(1));// getting particular index product
+		
+		String removeProject = product.remove(0);// Here i am removing particular index product
+		System.out.println("Removed Product is : "+ removeProject);
+		
+		System.out.println("Total Product is remaining is :"+ product.size());// Getting size of the product
+		
 	}
 }
--- Java_8_Programs/src/LanbdaExpression/LambdaImplementation.java
+++ Java_8_Programs/src/LanbdaExpression/LambdaImplementation.java
@@ -1,5 +1,8 @@
 package LanbdaExpression;
 
 public class LambdaImplementation {
-
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
 }
