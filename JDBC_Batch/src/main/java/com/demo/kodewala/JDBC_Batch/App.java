c0251f977c0a169bb835c8cb431ddd9ac1ba6507
MO SAIED <mosaied00786@gmail.com>
2025-05-21 12:46:49 +0530

    Add the JDBC Batch at first time

---
+++ JDBC_Batch/src/main/java/com/demo/kodewala/JDBC_Batch/App.java
@@ -0,0 +1,40 @@
+package com.demo.kodewala.JDBC_Batch;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class App {
+  public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	  
+	  
+	  //Load the driver class
+	  Class.forName("com.mysql.cj.jdbc.Driver");
+	  
+	  
+	 //Create Connection
+	  String url = "jdbc:mysql://localhost:3306/kodewala";
+		String userName = "root";
+		String password = "facebook";
+		
+		Connection con = DriverManager.getConnection(url,userName,password);
+		
+		//Create Prepared statement Object
+		
+		String inserSql= "insert into userinfo(username, password, hint_question, hint_answer) value(?,?,?,?)";
+		
+		PreparedStatement prepareStatement = con.prepareStatement(inserSql);
+		
+		for(int i=0; i<10; i++) {
+			prepareStatement.setString(1, "Mo Saied-"+i);
+			prepareStatement.setString(2, "mosaied@123-"+i);
+			prepareStatement.setString(3, "JDBC_Batch-"+i);
+			prepareStatement.setString(4, "Batches_process-"+i);
+			prepareStatement.addBatch();
+		}
+		
+		int[] batch = prepareStatement.executeBatch();
+		System.out.println(batch);
+  }
+}
